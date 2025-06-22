ce2270015f2363555a034b708c50f31011019081
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2025-06-22 00:16:55 -0700

    Add Suspense to analysis page

--- src/app/analysis/page.js
+++ src/app/analysis/page.js
@@ -1,14 +1,17 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Analysis() {
   const searchParams = useSearchParams();
 
   return (
-    <div>
-      <h1>Analysis</h1>
-      {searchParams.get("data")}
-    </div>
+    <Suspense>
+      <div>
+        <h1>Analysis</h1>
+        {searchParams.get("data")}
+      </div>
+    </Suspense>
   );
 }
