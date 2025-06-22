678b265bbfedaa8a24e77f6dd89ad1c5a9b6030a
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2025-06-22 00:22:01 -0700

    Try again

--- src/app/analysis/page.js
+++ src/app/analysis/page.js
@@ -1,15 +1,24 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function Analysis() {
+function Analysis() {
   const searchParams = useSearchParams();
   const data = searchParams.get("data");
 
   return (
     <div>
       <h1>Analysis</h1>
-      {}
+      {data}
     </div>
+  );
+}
+
+export default function AnalysisPage() {
+  return (
+    <Suspense>
+      <Analysis />
+    </Suspense>
   );
 }
