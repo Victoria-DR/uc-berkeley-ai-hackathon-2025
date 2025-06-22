781fd3c78cddb5ccd8d24a4964c9fd375db1b0e6
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2025-06-21 23:02:09 -0700

    Remove song title from prompt

--- src/_lib/gemini/analyze.js
+++ src/_lib/gemini/analyze.js
@@ -1,29 +1,23 @@
 import "dotenv/config";
-import {
-  GoogleGenAI,
-  // createUserContent,
-  // createPartFromUri,
-} from "@google/genai";
+import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
 
 export async function analyzeAudio(base64AudioFile) {
-  // const myfile = await ai.files.upload({
-  //   file: path,
-  //   config: { mimeType: "audio/wav" },
-  // });
-
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: [
-      { text: "Evaluate this recording of me playing piano and give me feedback on how to improve my playing. The name of the piece is Etude op. 25 no. 1 by Chopin." },
+      {
+        text: "Evaluate this recording of me playing piano and give me feedback on how to improve my playing.",
+      },
       {
         inlineData: {
           mimeType: "audio/wav",
           data: base64AudioFile,
-        }
-      }
-    ]
+        },
+      },
+    ],
   });
   console.log(response.text);
 }
+
