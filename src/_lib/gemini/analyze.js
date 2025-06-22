13db1d765111c8c2ea9e94421abfbdf8a9709361
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2025-06-21 23:43:17 -0700

    Improve prompt

--- src/_lib/gemini/analyze.js
+++ src/_lib/gemini/analyze.js
@@ -3,11 +3,23 @@ import { GoogleGenAI } from "@google/genai";
 const ai = new GoogleGenAI({ apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY });
 
 export async function analyzeAudio(instrument, songTitle, base64AudioFile) {
+  const config = {
+    thinkingConfig: {
+      thinkingBudget: -1,
+    },
+    responseMimeType: "text/plain",
+    systemInstruction: [
+      {
+        text: `You are an expert musician.`,
+      },
+    ],
+  };
+
   const result = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: [
       {
-        text: `Evaluate this recording of me playing ${songTitle} on ${instrument} and give me feedback on how to improve my playing.`,
+        text: `Analyze this recording of me playing ${songTitle} on the ${instrument} and provide 3 brief strengths and areas for improvement in bullet point form. Note my tempo, dynamics, phrasing, and articulation. Offer resources that illustrate what an ideal performance should sound like. Make reference to specific timestamps or sections of the piece.`,
       },
       {
         inlineData: {
@@ -18,6 +30,9 @@ export async function analyzeAudio(instrument, songTitle, base64AudioFile) {
     ],
   });
 
-  return result.candidates[0].content.parts[0].text ?? "Sorry, I couldn't analyze the audio file. Please try again later.";
+  return (
+    result.candidates[0].content.parts[0].text ??
+    "Sorry, I couldn't analyze the audio file. Please try again later."
+  );
 }
 
