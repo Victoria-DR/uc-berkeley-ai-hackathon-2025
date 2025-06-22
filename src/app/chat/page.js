d301c9e5a827e311d053f1bb86265a958accfae7
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2025-06-22 03:11:27 -0700

    Add chatbot

---
+++ src/app/chat/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Chat() {
+  return (
+    <iframe
+      src="https://live-audio-for-uc-berkeley-ai-hacka.vercel.app"
+      allow="microphone"
+      width="90%"
+      height="750px"
+      style={{ marginLeft: "5%" }}
+    ></iframe>
+  );
+}
+
